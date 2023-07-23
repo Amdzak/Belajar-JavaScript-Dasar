3b1be6526c1c18b67bb1b7e62d99b1eb7d961d5a
Amdzak <58507217+Amdzak@users.noreply.github.com>
2023-07-23 15:43:34 +0700

    Update [09]Array.js

--- [09]Array.js
+++ [09]Array.js
@@ -14,18 +14,40 @@
     pop() : menghilangkan nilai array di akhir
     unshift() : untuk menambah emelmen array di awal element atau di awal index bisa langsung menmabhakan lebih dari 1
     shift() : menghilangkan nilai array di awal
+    slice(indexAwal,IndexAkhir) :  unutk mengambil beberapa bagian dari array untuk menjadi bagian yang baru, tetapi untuk index akhir tidak akan di bawa oleh metdoh slicenya
+    splice(indexAwal,inginDihapusBrp,NilaiArray) : menambah atau menyisipkn nilai array di tengah,jika tidak ingin menghapus maka di beri nilai 0
+
 */
 
 var nilaiArray = [1,2,"Kucing","Salto",false,true];
 
+// .length()
 console.log(nilaiArray.length);
 console.log(nilaiArray.join()); // tanpa delimiter atau separator (pemisah)
 console.log(nilaiArray.join("-")); // dengan pemisah
+
+// .push()
 nilaiArray.push("lucu"); //akan berada pada index terakhir
 console.log(nilaiArray.join()); 
+
+// .pop()
 nilaiArray.pop()
 console.log(nilaiArray.join()); 
+
+// .unshift()
 nilaiArray.unshift("SIAPA AKU","lulu")
 console.log(nilaiArray.join()); 
+
+// .shift()
 nilaiArray.shift();
 console.log(nilaiArray.join()); 
+
+// .splice()
+nilaiArray.splice(2,0,"lucu");
+console.log(nilaiArray.join()); 
+
+// .slice() mengambil index 2,3 dari array
+var nilaiBaru = nilaiArray.slice(2,4);
+console.log(nilaiBaru.join()); 
+
+
